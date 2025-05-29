3152c8159f79fbe3e33fac1a889a0f67cbbcc7ff
scarecrow <918660602@qq.com>
2025-05-29 23:19:49 +0800

    init

--- src/main.js
+++ src/main.js
@@ -2,5 +2,6 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
+import axios from 'axios'
 
-createApp(App).use(ElementPlus).mount('#app')
+createApp(App).use(ElementPlus, { axios }).mount('#app')
